ac6a9de8c8bd24c7353053148bccb89bf7ebad2f
Jergan N Hector <jergannhector@pop-os.localdomain>
2023-01-12 10:15:05 -0400

    fixed folder import

--- src/backtests/strategies/RSI_Custom.py
+++ src/backtests/strategies/RSI_Custom.py
@@ -5,18 +5,18 @@ import datetime
 class RSICustom(bt.Strategy):
     def __init__(self):
         self.rsi = bt.indicators.RSI(self.data.close, period=30)
-        self.add_timer(when=bt.timer.SESSION_START,
-                       timer=True,
-                       cheat=False,
-                       offset=datetime.timedelta(),
-                       repeat=datetime.timedelta(),
-                       weekdays=[])
+        # self.add_timer(when=bt.timer.SESSION_START,
+        #                timer=True,
+        #                cheat=False,
+        #                offset=datetime.timedelta(),
+        #                repeat=datetime.timedelta(),
+        #                weekdays=[])
 
     def next(self):
         pos_size = self.broker.getvalue() // self.data.close[0]
 
-        if self.rsi < 30 and self.rsi < 20:  # BUY condition
-            if self.position and (self.rsi < 30 and self.rsi < 20):
+        if self.data.datetime.date().weekday() == 0:  # BUY condition
+            if self.position:
                 self.close()
             self.order = self.buy(size=pos_size)
 
