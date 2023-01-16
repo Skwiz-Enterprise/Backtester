bd85f810731dd06ad96044a3fdab99eb5e7b5eb7
Cesar Diaz <cesar@mistify.co>
2023-01-16 10:53:44 -0400

    added a backtest with summary output

--- src/backtests/strategies/RSI_MeanReversion.py
+++ src/backtests/strategies/RSI_MeanReversion.py
@@ -1,26 +1,27 @@
 import backtrader as bt
 
-
 class RSIMeanReversion(bt.Strategy):
     def __init__(self):
-        self.rsi = bt.indicators.RSI(self.data.close, period=21)
+        self.rsi = bt.indicators.RSI(self.data.close, lookback=21, period=21, safediv=True)
         self.wait = []
 
     def next(self):
-        pos_size = self.broker.getvalue() // self.data.close[0]
+        pos_size = int(self.broker.getvalue() / self.data.close[0])
+        # print(self.data.datetime.date(), self.broker.getvalue(), str(self.data.close[0]), pos_size)
 
-        print(self.rsi >= 50)
         if self.position:
-            print('We hve the meats')
+            # print('holding')
             self.wait.append(self.data.datetime.date())
             if (len(self.wait) >= 7):
+                # print('sell', pos_size)
                 self.wait = []
-                self.sell(size=pos_size)
+                self.close(size=pos_size)
 
-        if self.position and self.rsi > 80:
-            print('sell')
-            self.close(size=pos_size)
+        if self.position and self.rsi >= 80:
+            # print('sell')
             self.wait = []
+            self.close(size=pos_size)
 
-        if not self.position and self.rsi < 20:
+        if (not self.position) and self.rsi <= 20:
+            # print('buy', pos_size)
             self.buy(size=pos_size)
